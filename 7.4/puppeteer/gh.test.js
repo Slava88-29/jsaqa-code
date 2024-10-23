f36085a59ea4a68c05e3a5dea232fbc55822d710
Slava netologia <s-29@mail.ru>
2024-10-23 09:32:25 +0300

    Task 7.4

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,67 +1,123 @@
 let page;
-async function openPage(url) {
-  page = await browser.newPage();
-  await page.goto(url);
-  return page;
-}
-beforeEach(async () => {
-  page=await openPage("https://github.com/team");
-});
 
-afterEach(() => {
-  page.close();
-});
+async function openPage(url) {
+
+  page = await browser.newPage();
+
+  await page.goto(url);
+
+  return page;
+
+}
+
+
 
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
-    jest.setTimeout(10000)
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1',{timeout:5000});
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+
+  beforeEach(async () => {
+
+    page = await openPage("https://github.com/team");
+
   });
+
+
+
+  afterEach(() => {
+
+    page.close();
+
+  });
+
+
+
+  test("The h1 header content'", async () => {
+
+    const firstLink = await page.$("header div div a");
+
+    await firstLink.click();
+
+    await page.waitForSelector('h1');
+
+    const title2 = await page.title();
+
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+
+  }, 10000);
+
+
 
   test("The first link attribute", async () => {
-    jest.setTimeout(10000)
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
+
     expect(actual).toEqual("#start-of-content");
-  });
+
+  }, 10000);
+
+
 
   test("The page contains Sign in button", async () => {
-    jest.setTimeout(10000)
+
     const btnSelector = ".btn-large-mktg.btn-mktg";
+
     await page.waitForSelector(btnSelector, {
-      visible: true, timeout:3000
+
+      visible: true
+
     });
+
     const actual = await page.$eval(btnSelector, link => link.textContent);
+
     expect(actual).toContain("Get started with Team")
+
   });
-});
+
+}, 10000);
+
+
 
 test("Check title home page'", async () => {
+
   const newPage = await openPage("https://github.com");
-  jest.setTimeout(10000)
-  await newPage.waitForSelector('h1',{timeout:3000});
-  const actual = await newPage.$eval("h1", elem => elem.textContent);
-  expect(actual).toEqual("Search code, repositories, users, issues, pull requests...");
+
+  await newPage.waitForSelector('h1');
+
+  const title2 = await page.title();
+
+  expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
+
   await newPage.close();
-});
+
+}, 10000);
+
+
 
 test("Check title price page'", async () => {
-  const newPage = await openPage("https://github.com/pricing");
-  jest.setTimeout(10000)
-  await newPage.waitForSelector('h1',{timeout:3000});
-  const actual = await newPage.$eval("h1", elem => elem.textContent);
-  expect(actual).toEqual("Search code, repositories, users, issues, pull requests...");
-  await newPage.close();
-});
 
-test("Check title explore page'", async () => {
-  const newPage = await openPage("https://github.com/explore");
-  jest.setTimeout(10000)
-  await newPage.waitForSelector('h1',{timeout:3000});
-  const actual = await newPage.$eval("h1", elem => elem.textContent);
-  expect(actual).toEqual("Search code, repositories, users, issues, pull requests...");
+  const newPage = await openPage("https://github.com/pricing");
+
+  await newPage.waitForSelector('h1');
+
+  const title2 = await page.title();
+
+  expect(title2).toEqual("Pricing · Plans for every developer · GitHub");
+
   await newPage.close();
-});
+
+}, 10000);
+
+
+
+test("Check title features page'", async () => {
+
+  const newPage = await openPage("https://github.com/features");
+
+  await newPage.waitForSelector('h1');
+
+  const title2 = await page.title();
+
+  expect(title2).toEqual("GitHub Features · GitHub");
+
+  await newPage.close();
+
+}, 10000);
